bb4c3b849471367f5894ad0019ebdfd67665f4bc
S-Rabi <102955157+S-Rabi@users.noreply.github.com>
2024-03-20 16:36:23 +0100

    vercel eddited

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
